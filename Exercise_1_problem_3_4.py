288f8883c1e15090a034f382050848e8ac8e2ff1
YukaTakahashi15 <>
2021-06-06 17:42:02 +0000

    I restore data

--- Exercise_1_problem_3_4.py
+++ Exercise_1_problem_3_4.py
@@ -25,6 +25,7 @@ print(data.head())
 
 # 2: Select the 4 columns that contain coordinate information (**'from_x'**, **'from_y'**, **'to_x'**, **'to_y'**) and store them in variable **`data`** (i.e. update the data -variable  to contain only these four columns).
 # 
+data=data.filter(['x','y'])
 
 # YOUR CODE HERE 2 to set `data`
 
